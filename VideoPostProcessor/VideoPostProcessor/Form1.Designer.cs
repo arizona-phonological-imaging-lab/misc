2d45823ec0aa518be4d2388397267d533ac4ae01
z <zchap21@gmail.com>
2013-07-05 16:11:43 -0700

    post processing tool version 1.0
    takes AVI (or any video file) and decomposes into frames and audio using
    FFMPEG

---
+++ VideoPostProcessor/VideoPostProcessor/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace VideoPostProcessor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(47, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open Video File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(47, 87);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Process Video";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(179, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "no file selected";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 144);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Video Post Processing";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+
+
+    }
+}
+
---
+++ VideoPostProcessor/VideoPostProcessor/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace VideoPostProcessor
+{
+    
+    public partial class Form1 : Form
+    {
+
+        private string dtopfolder = System.Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        private string file;
+        private string fullPath;
+        private string fileName;
+        private string path;
+        private string frameFolder;
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            label1.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = dtopfolder;
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                file = openFileDialog1.FileName;
+                fullPath = openFileDialog1.FileName;
+                fileName = openFileDialog1.SafeFileName;
+                path = fullPath.Replace(fileName, "");
+                // create output folder for frames
+                frameFolder = path + "frames";
+                if (!(System.IO.Directory.Exists(frameFolder)))
+                {
+                    System.IO.Directory.CreateDirectory(frameFolder);
+                    //MessageBox.Show("created new directory at:\n\n" + frameFolder);
+                }
+                //MessageBox.Show("File: " + file + "\nFullPath: " + fullPath + "\nFilename: " + fileName + "\nPath: " + path + "\nDesktop Folder: " + dtopfolder);
+                button2.Enabled = true;
+                label1.Text = fileName;
+                label1.Enabled = true;
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            label1.Text = "Processing Video...";
+            // run ffmpeg command line script
+            Process process = new System.Diagnostics.Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            // ffmpeg command to extract video frames and audio file
+            // frames go in /frames folder created when opening the video
+            // audio file goes into video file path folder
+            startInfo.Arguments = "/C ffmpeg -i " + fullPath + " -r 30000/1001 -q:v 0 -f image2 " + frameFolder + "/frame-%07d.png -acodec pcm_s16le -ac 1 " + path + "audio.wav";
+            
+            // necessary for debugging in the event that files already exist and ffmpeg asks to overwrite
+            //startInfo.UseShellExecute = false;
+            //startInfo.RedirectStandardOutput = true;
+            //startInfo.RedirectStandardError = true;
+            process.StartInfo = startInfo;
+            process.Start();
+            
+            //while (!process.StandardOutput.EndOfStream)
+            //{
+            //    Console.WriteLine(process.StandardOutput.ReadLine());
+            //}
+            //string errOutput = process.StandardError.ReadToEnd();
+
+            process.WaitForExit();
+            //if (!string.IsNullOrEmpty(errOutput))
+            //{
+            //    MessageBox.Show(errOutput);
+            //}
+            label1.Text = "Processing Complete.";
+            process.Dispose();
+            // some way to show when process is done
+            //MessageBox.Show("Post-processing has completed");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
